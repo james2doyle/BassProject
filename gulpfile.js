941a48fd8ec8a2c2e4f206e03932a74b0e15e425
james2doyle <james2doyle@gmail.com>
2017-01-17 12:12:54 -0800

    convert gulpfile to more es6. run build

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,8 @@ const autoprefixer = require('gulp-autoprefixer');
 const header = require('gulp-header');
 const pkg = require('./package.json');
 
-const now = new Date();
 // Create an array with the current month, day and time
+const now = new Date();
 const date = `${now.getMonth() + 1}/${now.getDate()}/${now.getFullYear()}`;
 
 const banner = `/*!
@@ -22,7 +22,7 @@ const banner = `/*!
  * @copyright (c) <%= date %> <%= pkg.author.name %> (<%= pkg.author.url %>)
  */`;
 
-gulp.task('js', function() {
+gulp.task('js', () => {
   gulp.src('src/scripts/index.js')
     .pipe(sourcemaps.init())
     .pipe(browserify({
@@ -41,20 +41,20 @@ gulp.task('js', function() {
     .pipe(concat('script.js'))
     .pipe(sourcemaps.write())
     .pipe(header(banner, {
-      pkg: pkg,
-      date: date
+      pkg,
+      date
     }))
     .pipe(gulp.dest('js/'));
 });
 
-gulp.task('cssmin', function() {
+gulp.task('cssmin', () => {
   gulp.src('css/style.css')
     .pipe(cssmin())
     .pipe(concat('style.min.css'))
     .pipe(gulp.dest('css/'));
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', () => {
   gulp.src('src/styles/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass())
@@ -65,34 +65,34 @@ gulp.task('sass', function() {
     }))
     .pipe(concat('style.css'))
     .pipe(header(banner, {
-      pkg: pkg,
-      date: date
+      pkg,
+      date
     }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('css/'));
 });
 
-gulp.task('uglify', function() {
+gulp.task('uglify', () => {
   gulp.src('js/script.js')
     .pipe(uglify())
     .pipe(concat('script.min.js'))
     .pipe(header(banner, {
-      pkg: pkg,
-      date: date
+      pkg,
+      date
     }))
     .pipe(gulp.dest('js/'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
   gulp.watch('src/styles/*.scss', ['sass']);
   gulp.watch('src/styles/imports/*.scss', ['sass']);
   gulp.watch('src/scripts/**/*.js', ['js']);
 });
 
-gulp.task('default', ['sass', 'js'], function() {
+gulp.task('default', ['sass', 'js'], () => {
   // fired before 'finished' event
 });
 
-gulp.task('build', ['sass', 'cssmin', 'js', 'uglify'], function() {
+gulp.task('build', ['sass', 'cssmin', 'js', 'uglify'], () => {
   // fired before 'finished' event
 });
