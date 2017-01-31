d1a6f7576baccb4520768f5691f6b0652be48850
james2doyle <james2doyle@gmail.com>
2017-01-31 10:59:14 -0800

    better default container classes for index page

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </a>
 
 <div id="main" class="container" role="main" tabindex="-1">
-  <div class="clearfix">
+  <div class="clearfix max-width-4 mx-auto">
     <div class="col-12">
       <h1 class="mb0">BassProject</h1>
       <p>Based off of <a href="http://www.basscss.com/" target="_blank">Basscss</a> by <a href="http://jxnblk.com/" target="_blank">jxnblk</a></p>
