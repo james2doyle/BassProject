f58377f166f7452f135d4b6c9c3963d348654fb3
James Doyle <james2doyle@gmail.com>
2017-02-07 11:46:03 -0800

    Add "X-UA-Compatible" for IE

--- partials/meta.php
+++ partials/meta.php
@@ -1,3 +1,4 @@
 <meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="A starting point for new web projects using Basscss" />
 <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,user-scalable=yes">
